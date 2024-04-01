c30573b23e6e258471338b6aa4730f1c8ea35353
Darian Rodriguez <uceedmr@ucl.ac.uk>
2024-04-01 18:52:51 +0100

    Training with EfficientNet

--- helper_functions.py
+++ helper_functions.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 
 class TFRecordDatasetLoader:
+    
     """
     This class is responsible for loading data from TFRecord files and visualizing the distribution of classes.
 
@@ -173,6 +174,139 @@ class TFRecordDatasetLoader:
             empty_row_index = class_labels // num_cols
             empty_col_index = class_labels % num_cols
             axes[empty_row_index, empty_col_index].axis('off')
+
+        # Define the path where you want to save the plot
+        folder_path = "./figures"
+
+        # Save plot in the figures folder
+        plt.savefig(f"{folder_path}/{filename}.png")
+
+        # Explicitly close the figure
+        plt.close()
+
+
+        
+class Preprocessing:
+    """
+    Class containing methods for preprocessing dataset
+    """
+
+    def __init__(self,dataset):
+
+        # Initialize class variables here
+        self.dataset = dataset
+        self.num_samples = sum(1 for _ in self.dataset) # count samples in dataset
+
+
+    def split_data(self, train_percentage=0.8, valid_percentage=0.1, seed=None):
+
+        """
+        Split the dataset into training, validation, and test sets based on the provided percentages.
+
+        Args:
+            train_percentage (float): Percentage of the dataset to be used for training. Default is 0.8.
+            valid_percentage (float): Percentage of the dataset to be used for validation. Default is 0.1.
+            seed (int or None): Seed for shuffling the dataset. If None, shuffling will not be performed. Default is None.
+
+        Returns:
+            tuple: A tuple containing three datasets - training, validation, and test datasets.
+        """
+
+        # Shuffle dataset
+        if seed is not None:
+            self.dataset = self.dataset.shuffle(buffer_size=1000, seed=seed)
+        else:
+            self.dataset = self.dataset.shuffle(buffer_size=1000)
+
+
+        # Calculate dataset sizes
+        num_train_samples = int(train_percentage * self.num_samples)
+        num_valid_samples = int(valid_percentage * self.num_samples)
+        num_test_samples =self.num_samples - num_train_samples - num_valid_samples
+
+
+        # Split dataset
+        train_dataset = self.dataset.take(num_train_samples)
+        remaining_dataset = self.dataset.skip(num_train_samples)
+        valid_dataset = remaining_dataset.take(num_valid_samples)
+        test_dataset = remaining_dataset.skip(num_valid_samples)
+
+        return train_dataset, valid_dataset, test_dataset
+    
+    def resize_dataset(self, target_size):
+
+        """
+        Resize the dataset images to a target size.
+
+        Args:
+            target_size (tuple): Target size for resizing images, specified as (height, width).
+
+        Returns:
+            None
+        """
+
+        data = self.dataset
+
+        # Define a function to resize images
+        def resize_image(image, label, image_name):
+            resized_image = tf.image.resize(image, target_size)
+            return resized_image, label, image_name
+        
+        # Apply the resize function to each element in the training dataset
+        self.dataset = data.map(resize_image)
+
+    
+    def normalize_data(self):
+        """
+        Normalize image data in the dataset to the range [0, 1].
+
+        Returns:
+            None
+        """
+
+        def normalize_image(image, label, image_name):
+            # convert tensor from uint 8 to float 32 image
+            norm_image = tf.cast(image, tf.float32) / 255.0
+            return norm_image,label, image_name
+        
+        # Normalize
+        self.dataset = self.dataset.map(normalize_image)
+
+
+    def plot_distribution(self,data, title:str, abbrev_labels:dict,filename:str):
+
+        # Initialize a dictionary to store counts for each class label
+        class_counts = {}
+
+        # Iterate through the dataset and count occurrences of each class label
+        for image, label, _ in data:
+            label_ref = label.numpy()
+            class_counts[label_ref ] = class_counts.get(label_ref , 0) + 1
+
+        labels = list(class_counts.keys())
+        counts = list(class_counts.values())
+        labels,sorted_counts =  zip(*sorted(zip(labels, counts), key=lambda x: x[1], reverse=True)) # sort by counts
+        sorted_labels = [str(label) for label in labels] # convert labels to string to manage order
+
+        # Create bar plot
+        fig, ax = plt.subplots(figsize=(10, 6))
+        bar_container = ax.bar(sorted_labels, sorted_counts)
+
+        # Calculate total count
+        total_count = sum(sorted_counts)
+
+        # Calculate percentages
+        percentages = [count / total_count * 100 for count in sorted_counts]
+
+        #get labels abbrev
+        abb_labels = [abbrev_labels[abbrev] for abbrev in sorted_labels]
+        print(abb_labels)
+
+        # Plot number of each bar
+        ax.bar_label(bar_container, labels=["{:.2f}%".format(p) for p in percentages], fontsize=12)
+        ax.set_xticklabels(abb_labels, rotation=0)
+        ax.set_ylabel('Image Count')
+        ax.set_title(title)
 
         # Define the path where you want to save the plot
         folder_path = "./figures"
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Import Modules
-from helper_functions import TFRecordDatasetLoader
+from helper_functions import TFRecordDatasetLoader, Preprocessing
+from models import plot_accuracy_epochs, ModelTrainer, prediction,misclassified_samples
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import math
 
 # =====================================================================================================================
+# Load dataset
+
 # Define dataset paths
 TRAIN_TFREC_DIR = 'Datasets/train_tfrecords'
 LABELS_MAP_PATH = 'Datasets/label_num_to_disease_map.json'
@@ -21,8 +24,66 @@ data_loader.plot_image_distribution('class distribution')
 
 # show image sample per class
 data_loader.plot_images_per_class('class samples')
+
+# get labels with class names abbreviated
+abbrev_labels = data_loader.load_label_dictionary()
+
+
 # ======================================================================================================================
 # Data preprocessing
+
+# Create object for preprocessing
+preprocessor = Preprocessing(dataset)
+
+# Preprocess steps
+target_size = (512, 512) # original 512x 512
+preprocessor.resize_dataset(target_size)
+#preprocessor.normalize_data()
+
+# Split dataset
+train_dataset, valid_dataset, test_dataset = preprocessor.split_data(train_percentage=0.8, valid_percentage=0.1)
+
+# Create a dictionary to store the datasets
+datasets = {
+    'train': train_dataset,
+    'valid': valid_dataset,
+    'test': test_dataset
+}
+
+# ======================================================================================================================
+# Models Training
+
+trainer = ModelTrainer(batch_size=20, num_classes=5, dataset = datasets, labels = abbrev_labels)
+
+# Dictionary containing model fit parameters
+params = {
+    'epochs': 2,
+    'verbose': 1
+}
+
+# Train the model
+historic_data,model = trainer.train_efficientnet_transfer_learning(params,input_shape=(*target_size, 3))
+plot_accuracy_epochs(historic_data,'accuracy',"EfficientNetB3 learning")
+
+# Predict
+print("Prediction")
+predicted_labels,true_labels,images = prediction(trainer.valid_set,model)
+
+# Report Results
+print("Report")
+trainer.report_multi_results(predicted_labels,true_labels,"EfficientNet Report")
+
+
+# Evaluate model on validation set
+#loss, accuracy = model.evaluate(trainer.valid_set)
+#print("Validation Loss:", loss)
+#print("Validation Accuracy:", accuracy)
+
+# Plot some misclassified samples for error analysis
+misclassified_samples(predicted_labels,true_labels,images,"B3")
+
+
+
 #data_train, data_val, data_test = data_preprocessing(args...)
 # ======================================================================================================================
 # Task A
